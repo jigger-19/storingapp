a38d62916db7d23773b898dcaf766cf101852c79
jigger-19 <bjornscheepens19@gmail.com>
2024-03-01 10:01:45 +0100

    de formulier afhandeling van meldingen werkend gemaakt

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -4,14 +4,23 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
+$type = "";
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type)
+VALUES(:attractie, :capaciteit, :melder, :type)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":type" => $type
+]);
+
+header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
